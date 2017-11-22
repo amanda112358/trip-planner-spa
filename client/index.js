bda4d9f5aaf1af74afb524f58f3d157ca4378bae
Amanda Hussey <amandahussey@Amandas-MacBook-Air.local>
2017-11-22 11:31:04 -0600

    delete buttons fully functioning!

--- client/index.js
+++ client/index.js
@@ -15,3 +15,94 @@ const map = new mapboxgl.Map({
 
 const marker = buildMarker("activities", fullstackCoords);
 marker.addTo(map);
+
+window.onload = function () {
+  const hotelParent = document.getElementById('hotels-choices');
+  const restaurantParent = document.getElementById('restaurants-choices');
+  const activityParent = document.getElementById('activities-choices');
+
+  fetch("/api/")
+    .then(response => response.json())
+    .then(values => {
+      values[0].forEach(hotel => {
+        const hotelChild = document.createElement('option');
+        hotelChild.innerHTML = hotel.name;
+        hotelChild.coords = hotel.place.location;
+        hotelParent.append(hotelChild);
+      })
+      values[1].forEach(restaurant => {
+        const restaurantChild = document.createElement('option');
+        restaurantChild.text = restaurant.name;
+        restaurantChild.coords = restaurant.place.location;
+        restaurantParent.append(restaurantChild);
+      })
+      values[2].forEach(activity => {
+        const activityChild = document.createElement('option');
+        activityChild.text = activity.name;
+        activityChild.coords = activity.place.location;
+        activityParent.append(activityChild);
+      })
+  });
+}
+
+function addListItemAndMarker (dropDownId, buttonId, listParentId, markerType) {
+  const dropDown = document.getElementById(dropDownId);
+  const button = document.getElementById(buttonId);
+  button.addEventListener('click', (e) => {
+    const selected = dropDown.options[dropDown.selectedIndex];
+    const listParent = document.getElementById(listParentId);
+    const listChild = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'x';
+    deleteButton.className = 'delete-btn';
+    listChild.innerHTML = `${selected.text}`;
+    listChild.append(deleteButton);
+    listParent.append(listChild);
+    const coords = selected.coords;
+    const marker = buildMarker(markerType, coords);
+    marker.addTo(map);
+    deleteButton.addEventListener('click', (e) => {
+      listParent.removeChild(listChild);
+      marker.remove();
+    })
+  });
+}
+
+const arr = ['hotels', 'restaurants', 'activities']
+arr.forEach(item => addListItemAndMarker(`${item}-choices`, `${item}-add`, `${item}-list`, item));
+
+// const selectHotel = document.getElementById('hotels-choices');
+// const addHotel = document.getElementById('hotels-add');
+// addHotel.addEventListener('click', (e) => {
+//   const selectedHotel = selectHotel.options[selectHotel.selectedIndex];
+//   const hotelListParent = document.getElementById('hotels-list');
+//   const hotelListChild = document.createElement('li').text = selectedHotel.text;
+//   hotelListParent.append(hotelListChild);
+//   const hotelCoords = selectedHotel.coords;
+//   const hotelMarker = buildMarker('hotels', hotelCoords);
+//   hotelMarker.addTo(map);
+// });
+
+// const selectRestaurant = document.getElementById('restaurants-choices');
+// const addRestaurant = document.getElementById('restaurants-add');
+// addRestaurant.addEventListener('click', (e) => {
+//   const selectedRestaurant = selectRestaurant.options[selectRestaurant.selectedIndex];
+//   const restaurantListParent = document.getElementById('restaurants-list');
+//   const restaurantListChild = document.createElement('li').text = selectedRestaurant.text;
+//   restaurantListParent.append(restaurantListChild);
+//   const restaurantCoords = selectedRestaurant.coords;
+//   const restaurantMarker = buildMarker('restaurants', restaurantCoords);
+//   restaurantMarker.addTo(map);
+// });
+
+// const selectActivity = document.getElementById('activities-choices');
+// const addActivity = document.getElementById('activities-add');
+// addActivity.addEventListener('click', (e) => {
+//   const selectedActivity = selectActivity.options[selectActivity.selectedIndex];
+//   const activityListParent = document.getElementById('activities-list');
+//   const activityListChild = document.createElement('li').text = selectedActivity.text;
+//   activityListParent.append(activityListChild);
+//   const activityCoords = selectedActivity.coords;
+//   const activityMarker = buildMarker('activities', activityCoords);
+//   activityMarker.addTo(map);
+// });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { Place, Hotel, Restaurant, Activity } = require('../models');
 
 router.get('/', (req, res, next) => {
   res.sendFile('index.html');
+});
+
+router.get('/api', (req, res, next) => {
+
+  const hotels = Hotel.findAll({
+    include: [Place]
+  });
+
+  const restaurants = Restaurant.findAll({
+    include: [Place]
+  });
+
+  const activities = Activity.findAll({
+    include: [Place]
+  });
+
+  Promise.all([hotels, restaurants, activities])
+  .then(values => {
+    res.json(values);
+  });
+});
+
+router.get('/api/hotels', (req, res, next) => {
+  const hotels = Hotel.findAll({
+    include: [Place]
+  })
+  .then(hotels => {
+    res.json(hotels);
+  });
+});
+
+router.get('/api/restaurants', (req, res, next) => {
+  const restaurants = Restaurant.findAll({
+    include: [Place]
+  })
+  .then(restaurants => {
+    res.json(restaurants);
+  });
+});
+
+router.get('/api/activities', (req, res, next) => {
+  const activities = Activity.findAll({
+    include: [Place]
+  })
+  .then(activities => {
+    res.json(activities);
+  });
 });
 
 module.exports = router;
